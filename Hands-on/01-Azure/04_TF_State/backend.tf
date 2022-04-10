f6879dd0dc956645da1df7b6f3e52c8d453604cd
Atin Gupta <atingupta2005@gmail.com>
2022-04-10 16:28:33 +0530

    -

--- Hands-on/01-Azure/04_TF_State/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name   = "terraform-rg"
-    storage_account_name  = "terraformrg145231457"
-    container_name        = "tfstate"
-    key                   = "terraform.tfstate"
-  }
-}
